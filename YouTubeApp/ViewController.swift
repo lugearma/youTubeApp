287100f8992704e24745817284102bc4860b478d
Luis Arias <lugearma@gmail.com>
2017-06-09 14:03:23 -0500

    Initial Commit

---
+++ YouTubeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTubeApp
+//
+//  Created by Luis Arias on 6/9/17.
+//  Copyright © 2017 Luis Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
